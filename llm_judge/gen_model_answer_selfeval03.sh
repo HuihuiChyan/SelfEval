b9689ebc2cd0760801cb7c2e4605b7d66aad314d
huanghui_dxm <huanghui@duxiaoman.com>
2024-04-02 21:10:48 +0800

    fix some bugs

--- llm_judge/gen_model_answer_selfeval03.sh
+++ llm_judge/gen_model_answer_selfeval03.sh
@@ -7,7 +7,7 @@ ESTIMATION_MODE=logprobs-entropy
 python -u gen_model_answer_selfeval.py \
     --model-path ../models/$MODEL_NAME \
     --model-id $MODEL_NAME \
-    --bench_name $BENCH_NAME \
+    --bench-name $BENCH_NAME \
     --estimation-mode $ESTIMATION_MODE \
     --num-gpus-total 2
 
