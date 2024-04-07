b65498a465f94b2d4562cf82a892a7f3ca5961e2
huanghui_dxm <huanghui@duxiaoman.com>
2024-04-07 13:50:38 +0800

    fix some bugs

--- llm_judge/gen_model_answer.sh
+++ llm_judge/gen_model_answer.sh
@@ -6,12 +6,13 @@ BENCH_NAME=vicuna_bench
 python -u gen_model_answer.py \
     --model-path ../models/$MODEL_NAME \
     --bench-name $BENCH_NAME \
-    --model-id $MODEL_NAME
+    --model-id $MODEL_NAME \
+    --num-gpus-total 2
 
-python gen_judgment.py \
-    --model-list $MODEL_NAME \
-    --bench-name $BENCH_NAME \
-    --parallel 4 \
-    --judge-model gpt-4
+# python gen_judgment.py \
+#     --model-list $MODEL_NAME \
+#     --bench-name $BENCH_NAME \
+#     --parallel 4 \
+#     --judge-model gpt-4
 
 python show_result.py
