f3f9c709632d8b3798ad88130bab25b4b82d704d
huanghui_dxm <huanghui@duxiaoman.com>
2024-04-08 15:04:03 +0800

    fix some bugs

--- llm_judge/gen_model_answer_selfeval03.sh
+++ llm_judge/gen_model_answer_selfeval03.sh
@@ -3,7 +3,7 @@ export CUDA_VISIBLE_DEVICES=4,5
 export CUDA_LAUNCH_BLOCKING=1
 MODEL_NAME=vicuna-7b
 BENCH_NAME=vicuna_bench
-ESTIMATION_MODE=attention-entropy
+ESTIMATION_MODE=ensemble-logprobs-entropy
 python -u gen_model_answer_selfeval.py \
     --model-path ../models/$MODEL_NAME \
     --model-id $MODEL_NAME \
