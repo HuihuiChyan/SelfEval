6271ec7e367d637c3cb6609ebb4789e1183c4256
huanghui_dxm <huanghui@duxiaoman.com>
2024-04-09 14:43:03 +0800

    fix some bugs

--- llm_judge/gen_model_answer_selfeval04.sh
+++ llm_judge/gen_model_answer_selfeval04.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-export CUDA_VISIBLE_DEVICES=4,5,6,7
+export CUDA_VISIBLE_DEVICES=2,3
 export CUDA_LAUNCH_BLOCKING=1
 MODEL_NAME=llama2-7b-chat
-BENCH_NAME=vicuna_bench
+BENCH_NAME=mt_bench
 ESTIMATION_MODE=ensemble-prompt-logprobs-variance
 python -u gen_model_answer_selfeval.py \
     --model-path ../models/$MODEL_NAME \
     --model-id $MODEL_NAME \
     --bench-name $BENCH_NAME \
     --estimation-mode $ESTIMATION_MODE \
-    --num-gpus-total 4
+    --num-gpus-total 2
 
 python -u cal_correlation.py \
     --model_name $MODEL_NAME \
