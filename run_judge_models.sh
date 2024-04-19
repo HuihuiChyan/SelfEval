94dcdd1b58fe50a18d3ddc9109a129e7dc211dee
huanghui_dxm <huanghui@duxiaoman.com>
2024-04-19 11:00:02 +0800

    fix some bugs

--- run_judge_models.sh
+++ run_judge_models.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export CUDA_VISIBLE_DEVICES=0
 python -u run_judge_models.py \
-    --model-name-or-path "../CrossEval/models/Auto-J-13B" \
+    --model-name-or-path "./models/Auto-J-13B" \
     --model-type "auto-j" \
     --data-path-question ./llm_judge/data/vicuna_bench/question.jsonl \
     --data-path-answer ./llm_judge/data/vicuna_bench/model_answer/llama2-7b-chat-logprobs-entropy.jsonl
